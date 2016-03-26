2745d2b7c656b4927bbd885b67de725df67a96b8
Ron Chaplin <temprc1@carnegielearning.com>
2016-03-26 19:44:01 -0400

    Updating Tests

--- Tests/Ldap/LdapEntityManagerTest.php
+++ Tests/Ldap/LdapEntityManagerTest.php
@@ -1,19 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jgabler
- * Date: 9/17/15
- * Time: 11:09 AM
- */
-
 namespace CarnegieLearning\LdapOrmBundle\Tests\Ldap;
-
 
 use CarnegieLearning\LdapOrmBundle\Ldap\Converter;
 
 class LdapEntityManagerTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testAdDateConversion() {
         $adTimestamp = '130898490540000000.0Z';
         $decorator = Converter::fromAdDateTime($adTimestamp);
